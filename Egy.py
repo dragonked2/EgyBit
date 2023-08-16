9b7b225c4e198962c861c511d7ca0c2823c7962b
AliElTop <66541902+dragonked2@users.noreply.github.com>
2023-08-16 07:52:56 +0300

    Update Egy.py

--- Egy.py
+++ Egy.py
@@ -2,9 +2,9 @@ import ecdsa
 import hashlib
 import base58
 import secrets
-from bitcoin import *
+import re
 
-def generate_and_check(private_key, existing_addresses):
+def generate_and_check(private_key, existing_addresses, vanity_pattern=None):
     signing_key = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
     verifying_key = signing_key.get_verifying_key()
     public_key = b"\x04" + verifying_key.to_string()
@@ -12,7 +12,7 @@ def generate_and_check(private_key, existing_addresses):
     ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
     bitcoin_address = base58.b58encode_check(b"\x00" + ripemd160_hash).decode()
 
-    if bitcoin_address in existing_addresses:
+    if vanity_pattern and re.match(vanity_pattern, bitcoin_address):
         wif = base58.b58encode_check(b"\x80" + private_key).decode()
         match_info = (
             "Private Key (Hex): {}\n".format(private_key.hex()),
@@ -25,17 +25,20 @@ def generate_and_check(private_key, existing_addresses):
             matches_file.writelines(match_info)
             matches_file.flush()
 
-        print("Generated Bitcoin Address: {} (Match found!)".format(bitcoin_address))
+        print("Generated Vanity Bitcoin Address: {} (Match found!)".format(bitcoin_address))
+    elif bitcoin_address in existing_addresses:
+        print("Generated Bitcoin Address: {} (Match found in existing addresses)".format(bitcoin_address))
     else:
         print("Generated Bitcoin Address:", bitcoin_address)
 
 def main():
-    with open('output1.txt', 'r') as file:
+    vanity_pattern = "1Ali"  # Specify your desired vanity pattern here
+    with open('bito.txt', 'r') as file:
         existing_addresses = {line.strip() for line in file}
 
     while True:
         private_key = secrets.token_bytes(32)
-        generate_and_check(private_key, existing_addresses)
+        generate_and_check(private_key, existing_addresses, vanity_pattern)
 
 
 if __name__ == "__main__":
