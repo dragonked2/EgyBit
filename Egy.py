71d794e562deaa3833006ac8546ad05e9f13bec5
AliElTop <66541902+dragonked2@users.noreply.github.com>
2023-08-16 21:55:19 +0300

    Update Egy.py

--- Egy.py
+++ Egy.py
@@ -1,5 +1,7 @@
-import bitcoin
-from bitcoin import privtopub, pubtoaddr, encode_privkey
+import ecdsa
+import os
+import hashlib
+import base58
 import signal
 
 exit_flag = False
@@ -15,21 +17,38 @@ def check_for_match(address, addresses_to_match):
 
 def process_addresses(addresses_to_match, output_file):
     while not exit_flag:
-        private_key_hex = bitcoin.random_key()
-        private_key_wif = encode_privkey(private_key_hex, 'wif')
-        public_key_hex = privtopub(private_key_hex)
-        bitcoin_address = pubtoaddr(public_key_hex)
-        
+        # Generate a random private key
+        private_key = os.urandom(32)
+        private_key_hex = private_key.hex()
+
+        # Derive public key
+        sk = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
+        vk = sk.get_verifying_key()
+        public_key = b'\x04' + vk.to_string()
+        public_key_hex = public_key.hex()
+
+        # Calculate Bitcoin address
+        h = hashlib.sha256(public_key).digest()
+        ripe = hashlib.new('ripemd160')
+        ripe.update(h)
+        network_byte = b'\x00'  # Mainnet
+        extended_ripe = network_byte + ripe.digest()
+        checksum = hashlib.sha256(hashlib.sha256(extended_ripe).digest()).digest()[:4]
+        bitcoin_address = base58.b58encode(extended_ripe + checksum).decode('utf-8')
+
+        # Convert private key to WIF format
+        private_key_wif = base58.b58encode_check(b'\x80' + private_key).decode('utf-8')
+
         print("Private Key (Hex):", private_key_hex)
         print("Private Key (WIF):", private_key_wif)
         print("Bitcoin Address:", bitcoin_address)
-        
+
         if check_for_match(bitcoin_address, addresses_to_match):
             save_to_file(output_file, private_key_hex, private_key_wif, bitcoin_address)
             print("Data saved to bitcoin_addresses.txt")
 
 if __name__ == "__main__":
-    with open("filter.txt", "r") as file:
+    with open("ad.txt", "r") as file:
         addresses_to_match = set(file.read().splitlines())
 
     def signal_handler(sig, frame):
